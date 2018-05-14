0f3702bec6f8f66c43a38cb5d644c83c54f8fb05
Bas Kager <bas@kager.io>
2018-05-14 18:55:48 +0200

    Set up NPM and express, closes #11

---
+++ app/index.js
@@ -0,0 +1,35 @@
+const express = require('express'),
+app = express(),
+http = require('http').Server(app),
+debug = require('debug')('kager-server'),
+handlebars  = require('express-handlebars'),
+config = require("./config"),
+port = config.port;
+
+const amqp = require('amqplib/callback_api');
+
+app.engine('handlebars', handlebars({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+// Define directory from which static files are served
+app.use(express.static('public'));
+
+app.get('/', function(req, res) {
+    res.render('home');
+});
+
+app.get('/projects', function(req, res) {
+    res.render('projects');
+});
+
+// app.get('/project-detail', function(req, res) {
+//     res.render('project-detail');
+// });
+
+
+
+
+
+http.listen(port, function(){
+    console.log('Server listening on port ' + port)
+})
